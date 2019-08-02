d8a5275396c040f5afb99f9a63d8901e4133b82d
Gavin <gavinjboyd@gmail.com>
2019-08-02 21:12:17 +0900

    add scripts, basic generate working

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    public Texture2D mapTexture;
+
+    public ColorToPrefab[] colorMappings;
+
+    void Start()
+    {
+        GenerateLevel();
+    }
+
+    void GenerateLevel()
+    {
+        for(int x = 0; x < mapTexture.width; x++)
+        {
+            for (int y = 0; y < mapTexture.width; y++)
+            {
+
+                GenerateTile(x, y);
+
+            }
+        }
+    }
+
+    void GenerateTile(int x, int y)
+    {
+       Color pixelColor = mapTexture.GetPixel(x,y);
+        if (pixelColor.a == 0)
+        {
+            //ignore empty pixel
+            return;
+        }
+
+        foreach(ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x, y);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+
+    }
+
+
+}
