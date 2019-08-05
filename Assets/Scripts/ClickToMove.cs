1fb04fb1aab30c0fb76fff3d0bb32480885153b3
Gavin <gavinjboyd@gmail.com>
2019-08-05 18:54:02 +0900

    click to move working for player

---
+++ Assets/Scripts/ClickToMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class ClickToMove : MonoBehaviour
+{
+    private Transform target;
+    public GameObject moveTarget;
+    AIDestinationSetter aiDestination;
+
+    void Start()
+    {
+
+        aiDestination = GetComponent<AIDestinationSetter>();
+    }
+
+
+    void ClickToMoveOrder()
+    {
+        var mousePos = Input.mousePosition;
+
+
+        Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 worldPoint2d = new Vector2(worldPoint.x, worldPoint.y);
+
+        if (!target)
+        {
+            target = Instantiate(moveTarget).transform;
+
+            target.position = worldPoint2d;
+
+            aiDestination.target = target;
+
+        }
+        else
+        {
+            target.position = worldPoint2d;
+
+        }
+
+
+    }
+
+    void FixedUpdate()
+    {
+    //Check for click to move
+    if (Input.GetMouseButtonDown(0))
+    {
+            ClickToMoveOrder();
+
+    }
+
+    }
+}
