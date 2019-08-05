fa1843e9902049026e14fcfca732faf276bee251
Matthew Schwencke <matt.schwencke@gmail.com>
2019-08-05 12:38:29 +0900

    Battle Controller Turns
    Base turn system in place, using a placeholder entity at the moment.

---
+++ Assets/Scripts/BattleController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BattleController : MonoBehaviour {
+    public static BattleController Instance { get; private set; }
+
+    public int CurrentTick { get; private set; }
+    private List<Turn> turnQueue;
+
+    private void Awake() {
+        Instance = this;
+        turnQueue = new List<Turn>();
+    }
+
+    public void ScheduleTurn(Turn newTurn) {
+        //Set turn to current tick + delay
+        newTurn.SetTick(CurrentTick);
+        Debug.Log($"Turn's tick is {newTurn.Tick}. The Current Tick was {CurrentTick}, and the delay was {newTurn.TickDelay}.");
+        //Iterate over queue
+        //Insert before number bigger than tick
+
+        for (int i = 0; i < turnQueue.Count; i++) {
+            if (turnQueue[i].Tick > newTurn.Tick) {
+                turnQueue.Insert(i, newTurn);
+                Debug.Log($"New Turn inserted at index {i}.");
+                return;
+            }
+        }
+
+        turnQueue.Add(newTurn);
+        Debug.Log("New Turn Added to end.");
+    }
+
+    public void NextTurn() {
+        Debug.Log("Turn Ended, starting next turn.");
+        //Disable Control of Current Entity
+        /* ***************************************/
+
+        //Find the next turn
+        Turn currentTurn = turnQueue[0];
+        turnQueue.RemoveAt(0);
+        Debug.Log($"Next Turn is {currentTurn.ToString()}.");
+        //Change current Tick
+        CurrentTick = currentTurn.Tick;
+        Debug.Log($"Current tick changed to {CurrentTick}.");
+        //Give control to new Entity
+        /* ***************************************/
+    }
+
+    public void DebugPrintTurnQueue() {
+        int turnCount = 0;
+        foreach(Turn turn in turnQueue) {
+            Debug.Log($"Turn {turnCount++}, Entity {turn.Entity.ToString()} with Tick {turn.Tick}.");
+        }
+    }
+}
+
+[CustomEditor(typeof(BattleController))]
+public class TBattleControllerEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        BattleController myScript = (BattleController)target;
+        if (Application.isPlaying) {
+            if (GUILayout.Button("Next Turn")) {
+                myScript.NextTurn();
+            }
+            if (GUILayout.Button("Print Turn Queue")) {
+                myScript.DebugPrintTurnQueue();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TestTurnScheduler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestTurnScheduler : MonoBehaviour
+{
+    public int tickDelay;
+    public int entityID;
+
+    public void ScheduleTurn() {
+        Turn turn = new Turn(entityID, tickDelay);
+        BattleController.Instance.ScheduleTurn(turn);
+    }
+}
+
+[CustomEditor(typeof(TestTurnScheduler))]
+public class TurnSchedulerEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        TestTurnScheduler myScript = (TestTurnScheduler)target;
+        if (Application.isPlaying) {
+            if (GUILayout.Button("Schedule Turn")) {
+                myScript.ScheduleTurn();
+            }
+        }
+    }
+}
