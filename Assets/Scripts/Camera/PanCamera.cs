b58534522886ab6cce59d02a485eb49e37b2afc1
Matthew Schwencke <matt.schwencke@gmail.com>
2019-08-05 17:28:29 +0900

    Added Camera Movement
    Also moved some scripts around

---
+++ Assets/Scripts/Camera/PanCamera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanCamera : MonoBehaviour
+{
+    public float panSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = GetInputVector();
+        transform.Translate(move * panSpeed * Time.deltaTime);
+    }
+
+    Vector2 GetInputVector() {
+        float X = Input.GetAxis("Horizontal");
+        float Y = Input.GetAxis("Vertical");
+
+        return new Vector2(X, Y);
+    }
+}
