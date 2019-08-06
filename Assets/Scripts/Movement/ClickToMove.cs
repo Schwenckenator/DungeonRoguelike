ec0f2e72fff1fafa1d637259f039815f82fe95eb
Gavin <gavinjboyd@gmail.com>
2019-08-06 17:03:39 +0900

    reached destination updates

--- Assets/Scripts/Movement/ClickToMove.cs
+++ Assets/Scripts/Movement/ClickToMove.cs
@@ -6,13 +6,17 @@ public class ClickToMove : MonoBehaviour
     public GameObject parentObject;
     public GameObject moveTarget;
     AIDestinationSetter aiDestination;
+    AIPath aiPath;
 
     public GameObject distanceChecker1;
     public GameObject distanceChecker2;
 
-    public float maxDistanceCurrent = 3;
+    public float maxDistanceCurrent;//= 3;
     public float maxDistance1 = 1.5f;
     public float maxDistance2 = 3;
+
+    private bool seeking;
+    private EntityTurnScheduler turnScheduler;
 
     //Change this according to 
 
@@ -20,22 +24,36 @@ public class ClickToMove : MonoBehaviour
 
     void Start()
     {
-
+  
         aiDestination = parentObject.GetComponent<AIDestinationSetter>();
+        aiPath = parentObject.GetComponent<AIPath>();
 
+        turnScheduler = GetComponent<EntityTurnScheduler>();
         UpdateMaxDistance();
     }
 
     void UpdateMaxDistance()
     {
-        maxDistanceCurrent = maxDistance1;
+        if (turnScheduler.actionsRemaining > 1)
+        {
+            maxDistanceCurrent = maxDistance2;
 
+        }
+        else
+        {
+            maxDistanceCurrent = maxDistance1;
+
+        }
         Debug.Log("MaxDist " + maxDistanceCurrent);
     }
 
 
     void ClickToMoveOrder()
     {
+        //set to moving and seeking
+        seeking = true;
+
+
         var mousePos = Input.mousePosition;
         Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 worldPoint2d = new Vector2(worldPoint.x, worldPoint.y);
@@ -87,11 +105,22 @@ public class ClickToMove : MonoBehaviour
 
     void Update()
     {
+
+        if (seeking && aiPath.reachedEndOfPath)
+        {
+            seeking = false;
+            Debug.Log("Reached Destination");
+        }
         //Check for click to move
         if (Input.GetMouseButtonDown(0))
         {
-            ClickToMoveOrder();
+            //Prevent multiple clicks
+            if (!seeking)
+            {
+                ClickToMoveOrder();
+            }
 
         }
+
     }
 }
