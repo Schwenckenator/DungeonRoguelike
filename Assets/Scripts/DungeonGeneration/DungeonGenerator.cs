3415d2f52f9d7a032821e33d4596796c8b34a9ca
Matthew Schwencke <matt.schwencke@gmail.com>
2019-08-26 15:27:05 +0900

    Can now generate Multiple rooms
    They don't link up though

--- Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
+++ Assets/Scripts/DungeonGeneration/DungeonGenerator.cs
@@ -17,19 +17,17 @@ public class DungeonGenerator : MonoBehaviour
 {
     public Tilemap floorMap;
     public Tilemap wallMap;
-    public TileBase floorTile;
-    public TileBase wallTile;
+    //public TileBase floorTile;
+    //public TileBase wallTile;
     //public Texture2D design;
-
-    public RoomList rooms;
-
+    public int roomsPerLevel;
+    public RoomList roomContainer;
     public List<TileInfo> tilePairs;
 
     // Start is called before the first frame update
     void Start()
     {
-        GenerateMap(design, TileLayer.collision, wallMap, tilePairs);
-        GenerateMap(design, TileLayer.noCollision, floorMap, tilePairs);
+        GenerateLevel(roomsPerLevel);
 
         Invoke("Scan", 0.2f);
     }
@@ -45,11 +43,27 @@ public class DungeonGenerator : MonoBehaviour
             AstarPath.active.Scan();
         }
     }
+    /// <summary>
+    /// Generates an entire level of map
+    /// </summary>
+    void GenerateLevel(int numberOfRooms) {
+        Vector2Int offset = Vector2Int.zero;
+        for(int i=0; i<numberOfRooms; i++) {
+            offset = new Vector2Int(i*24,0);
+            int roomID = Random.Range(0, roomContainer.rooms.Length);
 
-    void GenerateMap(Texture2D image, TileLayer layer, Tilemap tileMap, List<TileInfo> tiles) {
+            GenerateRoom(roomContainer.rooms[roomID], TileLayer.collision, offset, wallMap, tilePairs);
+            GenerateRoom(roomContainer.rooms[roomID], TileLayer.noCollision, offset, floorMap, tilePairs);
+        }
+        
+    }
+    /// <summary>
+    /// Generates a single room
+    /// </summary>
+    void GenerateRoom(Texture2D image, TileLayer layer, Vector2Int offset, Tilemap tileMap, List<TileInfo> tiles) {
 
         for (int x = 0; x < image.width; x++) {
-            for (int y = 0; y < image.width; y++) {
+            for (int y = 0; y < image.height; y++) {
 
                 Color currentColour = image.GetPixel(x, y);
                 TileInfo tileInfo = null;
@@ -65,79 +79,79 @@ public class DungeonGenerator : MonoBehaviour
                     Debug.LogError("TileInfo is null!");
                 }
                 if(layer == tileInfo.layer) {
-                    tileMap.SetTile(new Vector3Int(x, y, 0), tileInfo.tile);
+                    tileMap.SetTile(new Vector3Int(x + offset.x, y + offset.y, 0), tileInfo.tile);
                 }
             }
         }
     }
 
-    int[,] GeneratePixelArray(Texture2D image, Color colour, bool avoidColour) {
-        int[,] newMap = new int[image.width + 1, image.width + 1];
+    //int[,] GeneratePixelArray(Texture2D image, Color colour, bool avoidColour) {
+    //    int[,] newMap = new int[image.width + 1, image.width + 1];
 
-        for (int x = 0; x < image.width; x++) {
-            for (int y = 0; y < image.width; y++) {
+    //    for (int x = 0; x < image.width; x++) {
+    //        for (int y = 0; y < image.width; y++) {
 
-                Color currentColour = image.GetPixel(x, y);
-                if (currentColour == colour && !avoidColour) {
-                    newMap[x, y] = 1;
-                } else {
-                    newMap[x, y] = 0;
-                }
+    //            Color currentColour = image.GetPixel(x, y);
+    //            if (currentColour == colour && !avoidColour) {
+    //                newMap[x, y] = 1;
+    //            } else {
+    //                newMap[x, y] = 0;
+    //            }
                 
-            }
-        }
+    //        }
+    //    }
 
-        return newMap;
-    }
+    //    return newMap;
+    //}
 
-    int[,] GenerateMapArray(int xSize, int ySize, bool filled) {
-        int[,] newMap = new int[xSize, ySize];
-        int defaultValue = filled ? 1 : 0;
+    //int[,] GenerateMapArray(int xSize, int ySize, bool filled) {
+    //    int[,] newMap = new int[xSize, ySize];
+    //    int defaultValue = filled ? 1 : 0;
 
-        for (int x = 0; x < xSize; x++) {
-            for (int y = 0; y < ySize; y++) {
-                newMap[x, y] = defaultValue;
-            }
-        }
+    //    for (int x = 0; x < xSize; x++) {
+    //        for (int y = 0; y < ySize; y++) {
+    //            newMap[x, y] = defaultValue;
+    //        }
+    //    }
 
-        return newMap;
-    }
-    int[,] GenerateMapArray(Texture2D image) {
-        int[,] newMap = new int[image.width+1, image.width+1];
+    //    return newMap;
+    //}
+    //int[,] GenerateMapArray(Texture2D image) {
+    //    int[,] newMap = new int[image.width+1, image.width+1];
 
-        for (int x = 0; x < image.width; x++) {
-            for (int y = 0; y < image.width; y++) {
+    //    for (int x = 0; x < image.width; x++) {
+    //        for (int y = 0; y < image.width; y++) {
 
-                bool draw = image.GetPixel(x, y).a != 0;
+    //            bool draw = image.GetPixel(x, y).a != 0;
 
-                newMap[x, y] = draw ? 1 : 0;
-            }
-        }
+    //            newMap[x, y] = draw ? 1 : 0;
+    //        }
+    //    }
 
-        return newMap;
-    }
-    int[,] GenerateMapArray(Texture2D image, bool opposite) {
-        int[,] newMap = new int[image.width + 1, image.width + 1];
+    //    return newMap;
+    //}
+    //int[,] GenerateMapArray(Texture2D image, bool opposite) {
+    //    int[,] newMap = new int[image.width + 1, image.width + 1];
 
-        for (int x = 0; x < image.width; x++) {
-            for (int y = 0; y < image.width; y++) {
+    //    for (int x = 0; x < image.width; x++) {
+    //        for (int y = 0; y < image.width; y++) {
 
-                bool draw = image.GetPixel(x, y).a == 0;
+    //            bool draw = image.GetPixel(x, y).a == 0;
 
-                newMap[x, y] = draw ? 1 : 0;
-            }
-        }
+    //            newMap[x, y] = draw ? 1 : 0;
+    //        }
+    //    }
 
-        return newMap;
-    }
+    //    return newMap;
+    //}
 
-    void RenderMap(int[,] map, Tilemap tileMap, TileBase tile){
-       for(int x = 0; x < map.GetUpperBound(0); x++) {
-            for( int y = 0; y< map.GetUpperBound(1); y++) {
-                if(map[x,y] == 1) {
-                    tileMap.SetTile(new Vector3Int(x, y, 0), tile);
-                }
-            }
-        }
-    }
+    //void RenderMap(int[,] map, Tilemap tileMap, TileBase tile){
+    //   for(int x = 0; x < map.GetUpperBound(0); x++) {
+    //        for( int y = 0; y< map.GetUpperBound(1); y++) {
+    //            if(map[x,y] == 1) {
+    //                tileMap.SetTile(new Vector3Int(x, y, 0), tile);
+    //            }
+    //        }
+    //    }
+    //}
 }
