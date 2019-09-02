7f39550beb8f99a0f6b08909aac0ec3ffd5d9f05
Matthew Schwencke <matt.schwencke@gmail.com>
2019-09-02 16:56:53 +0900

    Added zooming
    Also move speed adjusts with size, so the camera always seems to move at the same speed.

--- Assets/Scripts/Camera/PanCamera.cs
+++ Assets/Scripts/Camera/PanCamera.cs
@@ -4,19 +4,31 @@ using UnityEngine;
 
 public class PanCamera : MonoBehaviour
 {
-    public float panSpeed = 10;
+    public float panSpeed = 1f;
+    public float boost = 2f;
+    public float zoomSpeed = 1.5f;
+
+    private new Camera camera;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         Vector2 move = GetInputVector();
-        transform.Translate(move * panSpeed * Time.deltaTime);
+        //Camera size makes it relative to window size
+        transform.Translate(move * panSpeed * camera.orthographicSize * Time.deltaTime);
+
+        if(Input.GetAxis("Mouse ScrollWheel") < 0) {
+            camera.orthographicSize += zoomSpeed;
+        }
+        if (Input.GetAxis("Mouse ScrollWheel") > 0) {
+            camera.orthographicSize -= zoomSpeed;
+        }
     }
 
     Vector2 GetInputVector() {
