f27856588ad20f08545136630254c1961df698fe
Matthew Schwencke <matt.schwencke@gmail.com>
2020-01-11 16:47:36 +0900

    Bam, and the selectors are dynamic

--- Assets/Scripts/AbilityScripts/ConeAreaAbility.cs
+++ Assets/Scripts/AbilityScripts/ConeAreaAbility.cs
@@ -13,19 +13,53 @@ public class ConeAreaAbility : Ability {
     }
 
     public override void PrepareSelector(ref GameObject selector) {
-        selector.GetComponent<SpriteRenderer>().sprite = selectorSprite;
+        //selector.GetComponent<SpriteRenderer>().sprite = selectorSprite;
         selector.transform.localScale = new Vector3(range, range, 1);
-        selector.GetComponent<PolygonCollider2D>().points = CalculatePoints(32);
+        var points = CalculateArcPoints(16);
+        selector.GetComponent<PolygonCollider2D>().points = points.ToVector2s();
+        selector.GetComponent<MeshFilter>().mesh = CalculateMesh(points);
     }
 
-    private Vector2[] CalculatePoints(int arcPoints) {
+    private Vector3[] CalculateArcPoints(int arcPoints) {
         float theta;
-        Vector2[] points = new Vector2[arcPoints+2];
-        points[0] = new Vector2(0, 0); // First point is the origin
+        Vector3[] points = new Vector3[arcPoints+2];
+        points[0] = new Vector3(0, 0, 0); // First point is the origin
         for(int i = 0; i <= arcPoints; i++) {
             theta = (i) * (angle * Mathf.Deg2Rad) / (arcPoints);
             points[i+1] = new Vector3(Mathf.Cos(theta), Mathf.Sin(theta));
         }
         return points;
+    }
+
+    private Mesh CalculateMesh(Vector3[] points) {
+        List<Vector3> vertexList = new List<Vector3>();
+        List<int> triangleList = new List<int>();
+
+        vertexList.AddRange(points);
+
+        for (int i = 2; i < points.Length; i++) {
+            triangleList.Add(0);
+            triangleList.Add(i);
+            triangleList.Add(i - 1);
+        }
+        //triangleList.Add(0);
+        //triangleList.Add(1);
+        //triangleList.Add(circlePoints.Length);
+
+        Vector2[] uvs = new Vector2[vertexList.Count];
+
+        for (int i = 1; i < uvs.Length; i++) {
+            uvs[i] = new Vector2(vertexList[i].x, vertexList[i].y);
+        }
+
+
+        Mesh mesh = new Mesh {
+            name = "Cone",
+            vertices = vertexList.ToArray(),
+            triangles = triangleList.ToArray(),
+            uv = uvs
+        };
+
+        return mesh;
     }
 }
--- Assets/Scripts/AbilityScripts/SingleTargetAbility.cs
+++ Assets/Scripts/AbilityScripts/SingleTargetAbility.cs
@@ -5,21 +5,65 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New SingleTargetAbility", menuName = "Ability/Single Target Ability", order = 51)]
 public class SingleTargetAbility : Ability {
 
-    private readonly Vector2[] points = {
-        new Vector2(0.25f, 0f),
-        new Vector2(0f, 0.25f),
-        new Vector2(-0.25f, 0f),
-        new Vector2(0f, -0.25f)
-    };
+    //private readonly Vector2[] points = {
+    //    new Vector2(0.25f, 0f),
+    //    new Vector2(0f, 0.25f),
+    //    new Vector2(-0.25f, 0f),
+    //    new Vector2(0f, -0.25f)
+    //};
 
     public override void DisplayVisual(Vector2 position) {
         Instantiate(visual, position, Quaternion.identity);
     }
 
     public override void PrepareSelector(ref GameObject selector) {
-        selector.GetComponent<SpriteRenderer>().sprite = selectorSprite;
+        //selector.GetComponent<SpriteRenderer>().sprite = selectorSprite;
         selector.transform.localScale = Vector3.one;
-        selector.GetComponent<PolygonCollider2D>().points = points;
-        //selector.GetComponent<CircleCollider2D>().radius = 0.25f;
+        var points = CalculateCirclePoints(16);
+        selector.GetComponent<PolygonCollider2D>().points = points.ToVector2s();
+        selector.GetComponent<MeshFilter>().mesh = CalculateMesh(points);
+    }
+
+    private Mesh CalculateMesh(Vector3[] circlePoints) {
+        List<Vector3> vertexList = new List<Vector3>();
+        List<int> triangleList = new List<int>();
+
+        vertexList.Add(Vector3.zero); // Add centre point
+        vertexList.AddRange(circlePoints);
+
+        for (int i = 2; i < circlePoints.Length + 1; i++) {
+            triangleList.Add(0);
+            triangleList.Add(i);
+            triangleList.Add(i - 1);
+        }
+        triangleList.Add(0);
+        triangleList.Add(1);
+        triangleList.Add(circlePoints.Length);
+
+        Vector2[] uvs = new Vector2[vertexList.Count];
+
+        for (int i = 1; i < uvs.Length; i++) {
+            uvs[i] = new Vector2(vertexList[i].x, vertexList[i].y);
+        }
+
+
+        Mesh mesh = new Mesh {
+            name = "Circle",
+            vertices = vertexList.ToArray(),
+            triangles = triangleList.ToArray(),
+            uv = uvs
+        };
+
+        return mesh;
+    }
+
+    private Vector3[] CalculateCirclePoints(int totalPoints) {
+        float theta = 0;
+        Vector3[] points = new Vector3[totalPoints];
+        for (int i = 0; i < totalPoints; i++) {
+            theta = i * (2 * Mathf.PI) / totalPoints;
+            points[i] = new Vector3(Mathf.Cos(theta), Mathf.Sin(theta), 0) / 2;
+        }
+        return points;
     }
 }
