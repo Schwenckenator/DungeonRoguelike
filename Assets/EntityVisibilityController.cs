caa9d4fef9ac4cb122f52bb3f5d021a08866fbc6
Gavin <gavinjboyd@gmail.com>
2020-03-17 21:35:14 +0900

    create entityvisibilitycontroller

---
+++ Assets/EntityVisibilityController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityVisibilityController : MonoBehaviour
+{
+
+    //Reference for modifying layer options
+    public Canvas UICanvas;
+
+    public SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    public void DowngradeVisibilityLayer()
+    {
+
+        UICanvas.sortingLayerID = -1;
+        spriteRenderer.sortingLayerID = -1;
+
+    }
+    public void RestoreVisibilityLayer()
+    {
+
+        UICanvas.sortingLayerID = 0;
+        spriteRenderer.sortingLayerID = 0;
+
+    }
+
+ 
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+}
