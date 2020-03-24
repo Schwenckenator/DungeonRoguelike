c5cf31c767c29898d66a1fb54a79f3998207df8f
Gavin <gavinjboyd@gmail.com>
2020-03-24 22:48:27 +0900

    added subscription to turn, attempted to end life of the skill overtime, but it never decrements. more details on #51

--- Assets/Scripts/AbilityScripts/Effects/HideOTEffect.cs
+++ Assets/Scripts/AbilityScripts/Effects/HideOTEffect.cs
@@ -7,10 +7,32 @@ public class HideOTEffect : Effect {
     public int remainingActiveTurns;
     public float hiddenAlpha = 0.5f;
     public bool active;
+
+    private Entity entity;
     // Overtime Effect
 
+
+    public void OnEnable()
+    {
+   
+
+    }
+
+    public void OnDisable()
+    {
+        //Subscribe to player turn to decrement hide lifetime
+        GameEvents.current.onStartPlayerTurn -= DecrementEffectLifetime;
+    }
+
+
     public override void TriggerEffect(Entity target, int minValue, int maxValue) {
-        //target.Stats.AddOvertimeEffect(this);
+        // If first time subscribe and set entity parent
+        if (!entity)
+        {
+            //Subscribe to player turn to decrement hide lifetime
+            GameEvents.current.onStartPlayerTurn += DecrementEffectLifetime;
+            entity = target;
+        }
         EnterHide(target);
     }
     private void EnterHide(Entity me)
@@ -18,10 +40,23 @@ public class HideOTEffect : Effect {
         me.EntityVisibilityController.SetEntityAlpha(hiddenAlpha);
         active = true;
     }
-    public void ExitHide(Entity me)
+    public void ExitHide()
     {
-        me.EntityVisibilityController.SetEntityAlpha(1);
+        entity.EntityVisibilityController.SetEntityAlpha(1);
         active = false;
+    }
+
+    private void DecrementEffectLifetime(int entityID)
+    {
+        // Check only get responding to relevant entity
+        if (entityID == GetInstanceID())
+        { 
+            remainingActiveTurns -= 1;
+            if (remainingActiveTurns <= 0)
+            {
+                ExitHide();
+            }
+        }
     }
     private void Update()
     {
