dbffe81b946d4759335b31b386d3f7d5d042fb83
Gavin <gavinjboyd@gmail.com>
2020-03-24 22:02:59 +0900

    remove unneeded files

---
+++ Assets/Scripts/AbilityScripts/Effects/HideOTEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New HideOverTimeEffect", menuName = "Effect/Hide OverTime Effect", order = 52)]
+public class HideOTEffect : Effect {
+    public int remainingActiveTurns;
+    public float hiddenAlpha = 0.5f;
+    public bool active;
+    // Overtime Effect
+
+    public override void TriggerEffect(Entity target, int minValue, int maxValue) {
+        //target.Stats.AddOvertimeEffect(this);
+        EnterHide(target);
+    }
+    private void EnterHide(Entity me)
+    {
+        me.EntityVisibilityController.SetEntityAlpha(hiddenAlpha);
+        active = true;
+    }
+    public void ExitHide(Entity me)
+    {
+        me.EntityVisibilityController.SetEntityAlpha(1);
+        active = false;
+    }
+    private void Update()
+    {
+
+    }
+
+}
