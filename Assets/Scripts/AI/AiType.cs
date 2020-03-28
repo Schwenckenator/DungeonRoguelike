7f71222182f21db1c9e43aef68fce4d2ad22e2a6
Gavin <gavinjboyd@gmail.com>
2020-03-28 20:06:25 +0900

    added scriptable obj for ai type, and created decision function

---
+++ Assets/Scripts/AI/AiType.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New A.I. Type", menuName = "AI/New AIType", order = 53)]
+public class AiType : ScriptableObject
+{
+    public string aiTypeName;
+    //Eg front line would like to be at 1 distance when choosing attacks
+    public float preferredFightRange;
+    public float patrolRadius;
+    public float chaseRange;
+    public float endChaseRange;
+    public bool debug = true;
+
+
+
+    public void makeDecision(Entity me, Entity nearestEntity)
+    {
+        float distanceToEntity = (nearestEntity.transform.position - me.transform.position).magnitude;
+
+        bool nearestEntityExists = false;
+
+        if(nearestEntity!= null)
+        {
+            nearestEntityExists = true;
+        }
+
+
+        if (nearestEntityExists && distanceToEntity < preferredFightRange)
+        {
+            ShouldAttack(me, nearestEntity);
+        }
+        else if(nearestEntityExists && distanceToEntity <= chaseRange && distanceToEntity < endChaseRange)
+        {
+            ShouldChase(me, nearestEntity);
+
+        }
+        else if (patrolRadius > 0)
+        {
+            ShouldPatrol(me);
+
+        }
+        else
+        {
+            ShouldDoNothing(me);
+        }
+    }
+
+    public void ShouldChase(Entity me, Entity nearestEntity)
+    {
+        //TODO call AI Controller
+    }
+
+    public void ShouldAttack(Entity me, Entity nearestEntity)
+    {
+        //TODO call AI Controller
+    }
+    public void ShouldPatrol(Entity me)
+    {
+        //TODO call AI Controller
+    }
+    public void ShouldDoNothing(Entity me) {
+        //TODO call AI Controller
+    }
+}
