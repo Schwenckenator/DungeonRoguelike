e2b8aad691a36493f59e8819b6065ff6eaf0d93e
Matthew Schwencke <matt.schwencke@gmail.com>
2020-03-29 10:32:40 +0900

    Added a triangulator, but it sucks
    Doesn't work for our case.

--- Assets/Scripts/Pathfinding/PathBoundaryManager.cs
+++ Assets/Scripts/Pathfinding/PathBoundaryManager.cs
@@ -75,11 +75,43 @@ public class PathBoundaryManager : MonoBehaviour
         if (nodes.Length > 1) {
             Instance.meshFilters[index].gameObject.SetActive(true);
 
-            Instance.meshes[index] = SetupBoundary(nodes, Instance.meshes[index]);
+            Instance.meshes[index] = SetupMesh(nodes, Instance.meshes[index]);
             Instance.meshFilters[index].sharedMesh = Instance.meshes[index];
         } else {
             Instance.meshFilters[index].gameObject.SetActive(false);
         }
+    }
+
+    private static Mesh SetupMesh(PathNode[] nodes, Mesh mesh) {
+        var nodePositions = new List<Vector2>();
+
+        float offset = 0.25f;
+        foreach (var node in nodes) {
+            nodePositions.Add(new Vector2(node.position.x - offset, node.position.y - offset));
+            nodePositions.Add(new Vector2(node.position.x + offset, node.position.y - offset));
+            nodePositions.Add(new Vector2(node.position.x - offset, node.position.y + offset));
+            nodePositions.Add(new Vector2(node.position.x + offset, node.position.y + offset));
+        }
+
+        Triangulator triangulator = new Triangulator(nodePositions.ToArray());
+        int[] tris = triangulator.Triangulate();
+        var vertices = new List<Vector3>();
+
+        vertices.AddRange(nodePositions.Select(x => x.ToVector3()));
+
+        Vector2[] uvs = new Vector2[vertices.Count];
+
+        for (int i = 1; i < uvs.Length; i++) {
+            uvs[i] = new Vector2(vertices[i].x, vertices[i].y);
+        }
+        mesh.Clear();
+        mesh.SetVertices(vertices);
+        mesh.triangles = tris;
+        mesh.uv = uvs;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+        return mesh;
     }
 
     private static Mesh SetupBoundary(PathNode[] nodes, Mesh mesh) {
@@ -176,6 +208,8 @@ public class PathBoundaryManager : MonoBehaviour
         mesh.SetVertices(vertices);
         mesh.triangles = tris.ToArray();
         mesh.uv = uvs;
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
 
         return mesh;
     }
